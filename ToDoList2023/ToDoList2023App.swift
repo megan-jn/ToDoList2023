19b1146e1fc497f9235c9b2c575776dcbe0de31f
Megan Nguy <megannguy@gmail.com>
2023-05-15 16:49:24 -0700

    Initial Commit

---
+++ ToDoList2023/ToDoList2023App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2023App.swift
+//  ToDoList2023
+//
+//  Created by Megan on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
